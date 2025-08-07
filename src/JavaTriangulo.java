bc723318dfb801886d7d0a491f60da5117b5208c
João Vítor Bombo Guimarães <joaov.bombo.guimaraes@gmail.com>
2025-08-07 14:13:25 -0300

    Commit

---
+++ src/JavaTriangulo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class JavaTriangulo {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int altura, base;
+        float area;
+
+        System.out.println("Digite o altura do triangulo: ");
+        altura = teclado.nextInt();
+        System.out.println("Digite o base do triangulo: ");
+        base = teclado.nextInt();
+        area = 0.5f * altura * base;
+        System.out.println("Área do triangulo: " + area);
+    }
+}
