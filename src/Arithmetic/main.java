e996d86c63139dd0e261bf876b85f49c84cc236f
João Vítor Bombo Guimarães <joaov.bombo.guimaraes@gmail.com>
2025-08-13 15:39:42 -0300

    Commit

---
+++ src/Arithmetic/main.java
@@ -0,0 +1,39 @@
+package Arithmetic;
+
+public class main
+{
+    public static void main(String[] args)
+    {
+        // int x = 10;
+        //  int y = 2;
+        //  int z;
+
+        // z = x + y;
+        // z = x - y;
+        // z = x * y;
+        // z = x / y;
+        // z = x % y;
+
+
+        // x = x + y this is the same as x += y;
+
+        // x += y;
+        // x -= y;
+        // x *= y;
+        // x /= y;
+        // x %= y;
+
+
+
+        // x++;
+        // x--;
+
+        // Order of operations [P-E-M-D-A-S]
+
+        double result = 3 + 4 * (7 - 5) / 2.0;
+
+        System.out.println(result);
+
+    }
+
+}
