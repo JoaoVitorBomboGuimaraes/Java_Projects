2943957504632e0195f912e726290483193d3013
João Vítor Bombo Guimarães <joaov.bombo.guimaraes@gmail.com>
2025-08-20 15:28:32 -0300

    testando comandos de manipulação de string.

---
+++ src/teste_20_08_25/main.java
@@ -0,0 +1,22 @@
+package teste_20_08_25;
+
+public class main {
+    public static void main(String[] args) {
+
+        String name = "Joao";
+
+        // int length = name.length();
+        // char letter = name.charAt(0);
+        // int index = name.indexOf("J");
+        // int lastIndex = name.lastIndexOf("o");
+
+        //  name = name.toUpperCase();
+        //  name = name.toLowerCase();
+        //  name = name.trim();
+        // name = name.replace("o", "i");
+        // name.isEmpty();
+
+        System.out.println(name);
+
+    }
+}
